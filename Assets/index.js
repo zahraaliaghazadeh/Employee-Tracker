148335a6e8e52b16f16d7dec7415cabe9ab9e765
Zahra Ali Aghazadeh <Zahra@Zahras-MacBook-Pro.local>
2020-04-16 23:50:05 -0700

    added prompts

--- Assets/index.js
+++ Assets/index.js
@@ -7,71 +7,78 @@ var inquirer = require("inquirer");
 // create the connection information for the sql database
 var connection = mysql.createConnection({
     host: "localhost",
-  
+
     // Your port; if not 3306
     port: 3306,
-  
+
     // Your username
     user: "root",
-  
+
     // Your password
     password: "password",
     database: "employee_DB"
-  });
-  
-  // connect to the mysql server and sql database
-  connection.connect(function(err) {
+});
+
+// connect to the mysql server and sql database
+connection.connect(function (err) {
     if (err) throw err;
     // run the start function after the connection is made to prompt the user
     // start();
-  });
+});
 
-  inputArr=[
+inputArr = [
 
-{
-type:"list",
-message:"Select one option below",
-name:"action",
-// the * labeled ones are the minimum requirement
-choices:["*view All Employees","view All Employees By Department", "view All Employees By Manager", "*Add Employee","Remove Employee","*Update Employee Role","Update Employee Manager","*view All Roles","*Add department","*Add role","*view All departments"]
-},
+    {
+        type: "list",
+        message: "Select one option below",
+        name: "action",
+        // the * labeled ones are the minimum requirement
+        choices: ["*view All Employees", "view All Employees By Department", "view All Employees By Manager", "*Add Employee", "Remove Employee", "*Update Employee Role", "Update Employee Manager", "*view All Roles", "*Add department", "*Add role", "*view All departments"]
+    },
 
-{
-// add department
-    when: (response) => response.action === '*Add department'
-},
-{
-// add role
-when: (response) => response.action === '*Add role'
+    {
+        // add department
+        type: "input",
+        message: "Enter the Department",
+        name: "department",
+        when: (response) => response.action === '*Add department'
+    },
+    {
+        // add role
+        type: "input",
+        message: "Enter the role",
+        name: "role",
+        when: (response) => response.action === '*Add role'
 
-},
-{
-// add employee
+    },
+    {
+        // add employee
+        type: "input",
+        message: "Enter the Employee name",
+        name: "employee",
+        when: (response) => response.action === '*Add Employee'
 
-when: (response) => response.action === '*Add Employee'
+    },
+    {
+        // view department
+        
+        when: (response) => response.action === '*view All departments'
 
-},
-{
-    // view department
-    when: (response) => response.action === '*view All departments'
+    },
+    {
+        // view roles
+        when: (response) => response.action === '*view All Roles'
 
-},
-{
-    // view roles
-    when: (response) => response.action === '*view All Roles'
+    },
+    {
+        // view employees
+        when: (response) => response.action === '*view All Employees'
 
-},
-{
-    // view employees
-    when: (response) => response.action === '*view All Employees'
+    },
+    {
+        // update employee roles
+        when: (response) => response.action === '*Update Employee Role'
 
-},
-{
-    // update employee roles
-    when: (response) => response.action === '*Update Employee Role'
+    }
 
-}
-
-
-
-  ]
+]
